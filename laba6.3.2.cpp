cbf3f26db3412558244e5462c2c83d2845300df3
YarynaMryhlotska <89984528+YarynaMryhlotska@users.noreply.github.com>
2021-11-06 20:18:50 +0200

    21

--- laba6.3.2.cpp
+++ laba6.3.2.cpp
@@ -3,12 +3,33 @@
 #include <time.h>
 using namespace std;
 
+template <typename T >
+void Create(T* a, const T n, T Low, T High, T i)
+{
+	a[i] = Low + rand() % (High - Low + 1);
+	if (i < n - 1)
+		Create(a, n, Low, High, i + 1);
+}
+
 void Create(int* a, const int n, const int Low, const int High, int i)
 {
 	a[i] = Low + rand() % (High - Low + 1);
 	if (i < n - 1)
 		Create(a, n, Low, High, i + 1);
 }
+
+
+template <typename T >
+void Print(T* a, const T n, T i)
+{
+	cout << "a[" << setw(2) << i << " ] = " << setw(4) << a[i] << endl;
+	if (i < n - 1)
+		Print(a, n, i + 1);
+	else
+		cout << endl;
+}
+
+
 void Print(int* a, const int n, int i)
 {
 	cout << "a[" << setw(2) << i << " ] = " << setw(4) << a[i] << endl;
@@ -17,6 +38,16 @@ void Print(int* a, const int n, int i)
 	else
 		cout << endl;
 }
+
+template <typename T >
+const T& Sum(T* a, T n, T i)
+{
+	if (i < n)
+		return a[i] + Sum(a, n, i + 1);
+	else
+		return 0;
+}
+
 int Sum(int* a, const int n, int i)
 {
 	if (i < n)
